756b7a3d030a09a545d2048b807ba77a22223282
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-05-05 17:10:06 -0300

    incrementar, decrementar, resetar e alterar cores

--- 02-simple-counter/js/scripts.js
+++ 02-simple-counter/js/scripts.js
@@ -1,11 +1,30 @@
 const decrease = document.querySelector('.btn-decrease')
 const reset = document.querySelector('.btn-reset')
 const increase = document.querySelector('.btn-increase')
+const value = document.querySelector('#value')
 
-const saudacao = () => {
-  console.log('Olá, como você está?!')
+let counterVal = 0
+
+const updateDisplay = (val) => {
+  if(counterVal < 0) {
+    document.getElementById('value').style.color = 'red'
+  } else if (counterVal > 0) {
+    document.getElementById('value').style.color = 'green'
+  } else {
+    document.getElementById('value').style.color = '#102A42'
+  }
+  value.innerHTML = val
 }
 
 decrease.addEventListener('click', () => {
-  saudacao()
+  updateDisplay(--counterVal)
 })
+
+reset.addEventListener('click', () => {
+  updateDisplay(counterVal = 0)
+})
+
+increase.addEventListener('click', () => {
+  updateDisplay(++counterVal)
+})
+
