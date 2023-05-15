ac9c3f1d35a4ebddd5ed038a8150376a2409d17f
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-05-15 08:50:02 -0300

    button random

--- 03-reviews-quotes/js/script.js
+++ 03-reviews-quotes/js/script.js
@@ -1,31 +1,19 @@
 const randomButton = document.querySelector('#random-section')
-const firstSection = document.querySelector('.sec1')
-const secondSection = document.querySelector('.sec2')
-const thirdSection = document.querySelector('.sec3')
-const fourthSection = document.querySelector('.sec4')
 const allSections = document.querySelectorAll('.sec')
 
 const trade = () => {
-  // const loop = (Math.floor(Math.random() * 4))
-  // console.log(allSections)
-  // console.log(loop)
-
-  // for (let i = 0; i < 4; i++) {
-  //   if(loop != i){
-  //     allSections[i].style.display = "none";
-  //   } else{
-  //     allSections[i].style.display = "block";
-  //   }
-  // }
-
-  const sectionRandom = Math.floor(Math.random() * 4)
-  console.log(sectionRandom)
-
-  for (i = 0; i < allSections.length; i++) {
-    console.log(allSections[i])
+  const loop = (Math.floor(Math.random() * 4))
+  
+  for (let i = 0; i < 4; i++) {
+    if(loop != i){
+      allSections[i].style.display = "none";
+    } else{
+      allSections[i].style.display = "block";
+    }
   }
-
 }
+
+trade()
 
 randomButton.addEventListener('click', () => {
   trade()
